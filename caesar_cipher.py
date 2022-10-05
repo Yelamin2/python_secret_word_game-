fdd1dab1d96a3def78dd3b25e9ad4fc9dcd3b611
yelamin2 <yelamin2@yahoo.com>
2022-10-05 01:35:26 -0400

    second try

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,35 +1,38 @@
+from ast import Break, Return
+
+
 letters= ['abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ']
 letters_list= ''.join([ str(elem) for elem in letters])
-chiper_shift= int(input('Enter a number to shift your chipher with  '))
+cipher_shift= int(input('Enter a number to shift your cipher with  '))
+
 print("Print your secret message in letters and spaces only")
+
 secret_message = input('  ')
-first_shift= letters_list[26- chiper_shift:26]+letters_list[0:26 - chiper_shift]
-second_shift = letters_list[52- chiper_shift:52]+letters_list[26:52 - chiper_shift]
-chiper_letters = first_shift+second_shift
-# print(chiper_letters)
+first_shift= letters_list[26- cipher_shift:26]+letters_list[0:26 - cipher_shift]
+second_shift = letters_list[52- cipher_shift:52]+letters_list[26:52 - cipher_shift]
+cipher_letters = first_shift+second_shift
+# print(cipher_letters)
 secret_message = list(secret_message)
-chiper_message = []
+cipher_message = []
 for i in secret_message:
     new_index = letters_list.find(i)
     if new_index == -1:
-        chiper_message = chiper_message+[" "]
+        cipher_message = cipher_message+[" "]
     else:
-        chiper_message = chiper_message + [chiper_letters[new_index]]
+        cipher_message = cipher_message + [cipher_letters[new_index]]
 
-    
-
-chiper_message = ''.join([ str(elem) for elem in chiper_message])
-print('this is the new message"', chiper_message , '"')
-dechiper_message=[]
-for i in chiper_message:
-    new_index = chiper_letters.find(i)
+cipher_message = ''.join([ str(elem) for elem in cipher_message])
+print('this is the new message"', cipher_message , '"')
+decipher_message=[]
+for i in cipher_message:
+    new_index = cipher_letters.find(i)
     if new_index == -1:
-        dechiper_message = dechiper_message+[" "]
+        decipher_message = decipher_message+[" "]
     else:
-        dechiper_message = dechiper_message + [letters_list[new_index]]
+        decipher_message = decipher_message + [letters_list[new_index]]
 
     
-print(chiper_message)
-dechiper_message = ''.join([ str(elem) for elem in dechiper_message])
-print('this is the original message"', dechiper_message , '"')
+print(cipher_message)
+decipher_message = ''.join([ str(elem) for elem in decipher_message])
+print('this is the original message"', decipher_message , '"')
 
